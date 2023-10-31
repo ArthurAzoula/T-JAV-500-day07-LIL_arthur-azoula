d265442ee744dada1e3e8e17d8eb281f20dd9219
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-31 10:38:37 +0100

    add exo4

---
+++ Battalion.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Battalion {
+
+    private List<Character> characters;
+
+    public Battalion() {
+        characters = new ArrayList<>();
+    }
+
+    public void add(List<? extends Character> characters) {
+        this.characters.addAll(characters);
+    }
+
+    public void display() {
+        for (Character character : characters) {
+            System.out.println((character.getName()));
+        }
+    }
+}
